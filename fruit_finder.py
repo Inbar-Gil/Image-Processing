7047dcf545efb85e323a0f4dc46ce854a7954210
Inbar-Gil <59363167+Inbar-Gil@users.noreply.github.com>
2020-01-14 16:56:02 +0200

    Final Fruit Finding
    can identify multiple fruits,

--- fruit_finder.py
+++ fruit_finder.py
@@ -1,37 +1,38 @@
 import cv2
 import numpy as np
 
-WIN_LOC = [1000, 100]
 WIN_RAT = 16 / 9
-WIN_HEIGHT = 144
+WIN_HEIGHT = 240
 WIN_WIDTH = int(WIN_HEIGHT * WIN_RAT)
+WIN_LOC1 = [0, 0]
+WIN_LOC2 = [WIN_WIDTH, 0]
+WIN_LOC3 = [WIN_WIDTH * 2, 0]
+WIN_LOC4 = [0, WIN_HEIGHT]
+WIN_LOC5 = [WIN_WIDTH, WIN_HEIGHT]
+WIN_LOC6 = [2 * WIN_WIDTH, WIN_HEIGHT]
 WIN_SIZE = WIN_WIDTH, WIN_HEIGHT
 BLANK_IMAGE = cv2.imread('Blank_Image.png')
 cv2.imshow("TEST", BLANK_IMAGE)
 BLANK_IMAGE_GRAY = cv2.cvtColor(BLANK_IMAGE, cv2.COLOR_BGR2GRAY)
-BANNED_AREA = [[[-1, -1], [256, 17]], [[-1, 144], [20, 133]], [[256, 144], [180, 10]]]
-THRESH = 50
-IMAGE = cv2.imread('WATERMELON.png')
+BANNED_AREA = [[[-1, -1], [256, 72]], [[-1, 144], [20, 133]], [[256, 144], [180, 134]]]
+THRESH = 20
+IMAGE = cv2.imread('BOMB.png')
 
 
 def find_fruit(image, background):
-    gray_fig = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    diff = cv2.subtract(gray_fig, background)
-    ret_val, diff = cv2.threshold(diff, THRESH, 255, cv2.THRESH_BINARY)
-    locs = cv2.findNonZero(diff)
-    if locs is not None:
-        min_max_vals = find_min_max_vals(locs)
-        if min_max_vals is not None:
-            min_x, min_y, max_x, max_y = min_max_vals
-            update = cv2.rectangle(image, (min_x, min_y), (max_x, max_y), (0, 0, 255))
-        else:
-            update = image
-    else:
-        update = image
-    return update
+    # edits an image using grayscale, morphology and contours
+    initial_image = image.copy()
+    gray_fig = show_grayscale_img(image)
+    diff = show_diff_img(gray_fig, background)
+    diff = show_threshed_img(diff)
+    opening, fruit_locs = eliminate_noise(diff, it_close=3, it_open=1)
+    contours = add_contours(image, initial_image, opening)
+    update = show_final_image(image, 0)
+    return update, fruit_locs, contours
 
 
 def find_min_max_vals(list_3d):
+    # finds the min max values of a 2d list contained in a 3d list
     x = np.zeros(len(list_3d))
     y = np.zeros(len(list_3d))
     for i in range(len(list_3d)):
@@ -52,6 +53,7 @@ def find_min_max_vals(list_3d):
 
 
 def check_banned_areas(x, y):
+    # confirms points are in allowed areas
     for rect in BANNED_AREA:
         if check_in_rectangle(rect[0], rect[1], x, y):
             return True
@@ -59,6 +61,7 @@ def check_banned_areas(x, y):
 
 
 def check_in_rectangle(rect_start, rect_end, x, y):
+    # checks if a point is in a rectangle
     x_start = min(rect_start[0], rect_end[0])
     x_end = max(rect_start[0], rect_end[0])
     y_start = min(rect_start[1], rect_end[1])
@@ -67,11 +70,70 @@ def check_in_rectangle(rect_start, rect_end, x, y):
 
 
 def create_window(win_name, win_size, win_loc):
+    # creates a window to display an image
     cv2.namedWindow(win_name, flags=cv2.WINDOW_KEEPRATIO)
     win_x, win_y = win_loc
     cv2.moveWindow(win_name, win_x, win_y)
     win_width, win_height = win_size
     cv2.resizeWindow(win_name, win_width, win_height)
+
+
+def show_grayscale_img(image):
+    # edits an image into grayscale channel
+    gray_fig = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    create_window("gray_fig", WIN_SIZE, WIN_LOC2)
+    cv2.imshow("gray_fig", gray_fig)
+    return gray_fig
+
+
+def show_diff_img(image, background):
+    # subtracts a background from an image
+    diff = cv2.subtract(image, background)
+    create_window("image difference", WIN_SIZE, WIN_LOC3)
+    cv2.imshow("image difference", diff)
+    return diff
+
+
+def show_threshed_img(diff, thresh=THRESH):
+    # edits an image with thresholding
+    ret_val, diff = cv2.threshold(diff, thresh, 255, cv2.THRESH_BINARY)
+    create_window("threshed", WIN_SIZE, WIN_LOC4)
+    cv2.imshow("threshed", diff)
+    return diff
+
+
+def eliminate_noise(diff, it_open=1, it_close=1):
+    # dilates and erodes an image to close holes and eliminate noise
+    kernel = np.ones((5, 5), np.uint8)
+    closing = cv2.morphologyEx(diff, cv2.MORPH_CLOSE, kernel, iterations=it_close)
+    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel, iterations=it_open)
+    create_window("no noise", WIN_SIZE, WIN_LOC6)
+    cv2.imshow("no noise", opening)
+    fruit_locs = cv2.findNonZero(opening)
+    return opening, fruit_locs
+
+
+def add_contours(image, initial_image, opening):
+    # adds the edges of objects to an image and the rectangles that contain them to the origin
+    contours, hierarchy = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    if contours is not None:
+        for contour in contours:
+            min_max_vals = find_min_max_vals(contour)
+            if min_max_vals is not None:
+                min_x, min_y, max_x, max_y = min_max_vals
+                cv2.rectangle(image, (min_x, min_y), (max_x, max_y), (0, 0, 255))
+                contoured = cv2.drawContours(initial_image, [contour], 0, (0, 0, 255))
+                create_window("contoured", WIN_SIZE, WIN_LOC5)
+                cv2.imshow("contoured", contoured)
+    return contours
+
+
+def show_final_image(image, delay):
+    # shows an image after all editing
+    update = image
+    create_window("edited", WIN_SIZE, WIN_LOC1)
+    cv2.imshow("edited", update)
+    return update
 
 
 find_fruit(IMAGE, BLANK_IMAGE_GRAY)
--- video_reader.py
+++ video_reader.py
@@ -4,9 +4,9 @@ import timeit
 
 FPS = 29
 FILE_TYPE = '.mp4'
-WIN_LOC = [300, 100]
+WIN_LOC = [0, 0]
 WIN_RAT = 16 / 9
-WIN_HEIGHT = 500
+WIN_HEIGHT = 240
 WIN_WIDTH = int(WIN_HEIGHT * WIN_RAT)
 WIN_SIZE = WIN_WIDTH, WIN_HEIGHT
 BLANK_IMAGE = cv2.imread('Blank_Image.png')
@@ -14,6 +14,7 @@ BLANK_IMAGE_GRAY = cv2.cvtColor(BLANK_IMAGE, cv2.COLOR_BGR2GRAY)
 
 
 def read_file(file_name):
+    # checks if a video is opened
     if FILE_TYPE not in file_name:
         print(f'The file must be of type{FILE_TYPE}')
         return False
@@ -25,47 +26,37 @@ def read_file(file_name):
 
 
 def load_vid(file='', delay=0):
+    # loads a video in the cv2 library
     captured_vid = read_file(file)
     while not captured_vid:
         file = input("Input File Name")
         captured_vid = read_file(file)
-        display_vid(captured_vid, delay)
+        if captured_vid:
+            display_vid(captured_vid, delay)
 
 
-def display_vid(video, delay):
-    now = timeit.timeit()
+def display_vid(video_in, delay):
+    # this function edits the video frame by frame and rewrites it
     frame_counter = 0
     background = BLANK_IMAGE_GRAY
-    prev_frame = BLANK_IMAGE
-    video_out = cv2.VideoWriter('Edited_fruit_ninja.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 29, (256, 144))
-    while video.isOpened():
-        ret_val, frame = video.read()
+    prev_frame = None
+    video_out = cv2.VideoWriter('Edited_fruit_ninja_2.0.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 29,
+                                (256, 144))
+    while video_in.isOpened():
+        ret_val, frame = video_in.read()
         if ret_val:
-            if find_background(prev_frame, frame):
-                background = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-            frame_counter += 1
-            time = calc_time(frame_counter)
-            """win_name = f"Frame #{frame_counter}  Time Passed:{time} s"
-            #create_window(win_name, WIN_SIZE, WIN_LOC)"""
-            fruit_found = find_fruit(frame, background)
-            video_out.write(fruit_found)
-            k = cv2.waitKey(delay) & 0xFF
-            if k == ord('s'):
-                cv2.imwrite('WATERMELON.png', frame)
-            if k == 27:
-                cv2.destroyAllWindows()
+            if user_input(frame_counter, delay, frame) == -1:
                 break
-            """if frame_counter == 200:
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                break"""
-            prev_frame = frame
-            if time > 30:
+            background = check_for_background(frame, prev_frame, background)
+            frame_counter = display_images(frame_counter, frame, background)
+            video_out.write(frame)
+            if user_input(frame_counter, delay, frame) == -1:
                 break
+            cv2.destroyAllWindows()
+            prev_frame = frame.copy()
         else:
             break
-    video.release()
-    video_out.release()
+    release_videos(video_in, video_out)
 
 
 def calc_time(frames):
@@ -73,6 +64,7 @@ def calc_time(frames):
 
 
 def create_window(win_name, win_size, win_loc):
+    # creates a window to display an image
     cv2.namedWindow(win_name, flags=cv2.WINDOW_KEEPRATIO)
     win_x, win_y = win_loc
     cv2.moveWindow(win_name, win_x, win_y)
@@ -81,6 +73,7 @@ def create_window(win_name, win_size, win_loc):
 
 
 def find_background(frame_1, frame_2):
+    # checks whether the background of a video has changed
     gray_1 = cv2.cvtColor(frame_1, cv2.COLOR_BGR2GRAY)
     gray_2 = cv2.cvtColor(frame_2, cv2.COLOR_BGR2GRAY)
     diff = cv2.subtract(gray_1, gray_2)
@@ -90,6 +83,45 @@ def find_background(frame_1, frame_2):
     if len(diff_locs) < 20:
         return True
     return False
+
+
+def check_for_background(frame, prev_frame, background):
+    # manages the change of background
+    if prev_frame is None:
+        prev_frame = frame
+    if find_background(prev_frame, frame):
+        background = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    return background
+
+
+def display_images(frame_counter, frame, background):
+    # edits a specific image and displays the whole editing process
+    time = calc_time(frame_counter)
+    win_name = f"Frame #{frame_counter}  Time Passed:{time} s"
+    create_window(win_name, WIN_SIZE, WIN_LOC)
+    fruit_found, fruit_locs, contours = find_fruit(frame, background)
+    cv2.imshow(win_name, fruit_found)
+    return frame_counter + 1
+
+
+def user_input(frame_counter, delay, frame):
+    # checks and acts on user input
+    k = cv2.waitKey(delay) & 0xFF
+    if k == ord('s'):
+        cv2.imwrite('BOMB.png', frame)
+    if k == 27:
+        cv2.destroyAllWindows()
+        return -1
+    if frame_counter == 2000:
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return -1
+
+
+def release_videos(video_in, video_out):
+    # releases and saves both input and output videos
+    video_in.release()
+    video_out.release()
 
 
 if __name__ == "__main__":
